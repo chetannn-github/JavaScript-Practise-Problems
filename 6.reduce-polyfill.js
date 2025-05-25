cc1a254c75f0c69f70983892ef7bcb2ebd6fe561
chetannn-github <chetan.rajawat25@gmail.com>
2025-05-25 21:15:35 +0530

    Implement myReduce polyfill

---
+++ 6.reduce-polyfill.js
@@ -0,0 +1,18 @@
+Array.prototype.myReduce = function (callBackFn, intial) {
+    if(typeof callBackFn !== 'function') {
+        return new TypeError("call back fn is not a function ");
+    }
+
+    if(!this.length && !intial) {
+        return new TypeError("reduce for empty array with no intial value");
+    }
+
+    let acc = intial ? intial : this[0];
+    const startIdx = intial ? 0 : 1;
+    
+    for(let i = startIdx ; i< this.length; i++) {
+        acc = callBackFn (acc, this[i], i, this);
+    }
+
+    return acc;
+}
