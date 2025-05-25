45591a80ed99b33505919f99fc27261bb176571f
chetannn-github <chetan.rajawat25@gmail.com>
2025-05-25 17:24:28 +0530

    Implement currying function variants.

---
+++ 3.currying-problems.js
@@ -0,0 +1,36 @@
+// A => fixed length args
+// let sum = (a) => (b) => a+b;
+
+
+
+// B => infinte single args 
+// let sum = (a) => {
+//     return (b) => {
+//         if(b == undefined) {
+//             return a;
+//         }else {
+//             return sum (a+b);
+//         }
+//     }
+// }
+
+// console.log(sum(1)(1)(1)(2)(1)());
+
+
+
+// C => infinte kisi me kitne bhii args 
+let sum = (...a) => {
+    return (...b) => {
+        if(b.length == 0) {
+            return a.reduce((acc, curr)=>(acc + curr), 0);
+        }else {
+            let totalSum =a.reduce((acc, curr)=>(acc + curr), 0) + b.reduce((acc, curr)=>(acc + curr), 0);
+            return sum(totalSum);
+        }
+    }
+}
+
+console.log(sum(1,10,2)());
+
+
+
