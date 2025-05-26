dfe520333f833e6f9fa23336f883a36d83cbc090
chetannn-github <chetan.rajawat25@gmail.com>
2025-05-26 17:08:02 +0530

    Add myCall method to Function prototype for custom call functionality

--- index.js
+++ index.js
@@ -3,16 +3,19 @@ export { deepClone } from './src/2.deep-clone-objects.js';
 export { sum as currySum } from './src/3.currying-problems.js';
 export  {myNew} from './src/9.new-keyword-polyfill.js';
 
+
 import './src/4.map-polyfill.js';
 import './src/5.filter-polyfill.js';
 import './src/6.reduce-polyfill.js';
 import './src/7.slice-polyfill.js';
 import './src/8.flat-polyfill.js';
 import './src/10.object.create-pollyfill.js';
+import './src/11.call-polyfill.js'
+import './src/12.apply-polyfill.js'
 
 
 
-console.log('Polyfills for map, filter, reduce, slice, object.create, and flat have been loaded.');
+console.log('Polyfills for map, filter, reduce, slice, object.create, call, apply and flat have been loaded.');
 
 
 // console.log([2,1,2].myReduce((acc, val) => acc + val, 0)); // 5
---
+++ src/11.call-polyfill.js
@@ -0,0 +1,44 @@
+
+// ! usage of call and apply method
+// let obj = {
+//     name : "chetan singh ",
+//     age : 22
+// }
+
+// function fn (greet) {
+//     // console.log(this);
+//     console.log(`${greet} from ${this?.name} and my age is ${this?.age}`  );
+// }
+
+
+// // console.log(fn("namaste"));
+// console.log(fn.call(obj,"namastee"));
+// console.log(fn.apply(obj,["namastee"]));
+
+
+Function.prototype.myCall = function (thisArg, ...args) {
+    thisArg = thisArg || globalThis;
+    let sym = Symbol();
+    thisArg[sym] = this; 
+    
+    const result = thisArg[sym](...args);
+    delete thisArg[sym]
+
+    return result;
+}
+
+
+// let obj = {
+//     name : "chetan singh ",
+//     age : 22
+// }
+
+// function fn (greet) {
+//     console.log(this);
+//     console.log(`${greet} from ${this?.name} and my age is ${this?.age}`  );
+// }
+
+
+// // console.log(fn("namaste"));
+// fn.myCall(obj,"namastee");
+// console.log(fn.apply(obj,["namastee"]));
